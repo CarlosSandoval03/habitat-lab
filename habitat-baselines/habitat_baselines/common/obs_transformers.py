E2E implementation: learning fixed. Backward and optimization working. Problem with vanishing gradients

habitat-baselines/habitat_baselines/common/obs_transformers.py
@@ -1242,18 +1242,58 @@ def apply_obs_transforms_obs_space(
 
 def apply_obs_transforms_batch_video(
     batch: Dict[str, torch.Tensor],
-    obs_transforms: Iterable[ObservationTransformer]
+    obs_transforms: Iterable[ObservationTransformer],
+    decoder
 ) -> Tuple[Dict[str, torch.Tensor], List[Dict[str, torch.Tensor]]]:
     batch_all=[]
+    # """
+    if isinstance(decoder, ObservationTransformer):
+        reconstruction = decoder(batch)
+        if reconstruction['rgb'].shape[-1] == 1:
+            new_image_recon = reconstruction['rgb'][0, :, :, 0].detach().cpu().numpy()
+            min_val = np.min(new_image_recon)
+            max_val = np.max(new_image_recon)
+            if (max_val - min_val) != 0:
+                normalized_img_tensor = (new_image_recon - min_val) / (max_val - min_val)
+            else:
+                normalized_img_tensor = new_image_recon / np.max(new_image_recon)
+        else:
+            new_image_recon = reconstruction['rgb'][0, :, :, :].detach().cpu().numpy()
+            min_val = np.min(new_image_recon)
+            max_val = np.max(new_image_recon)
+            if (max_val - min_val) != 0:
+                normalized_img_tensor = (new_image_recon - min_val) / (max_val - min_val)
+            else:
+                normalized_img_tensor = new_image_recon / np.max(new_image_recon)
+        plt.imsave('/home/carsan/Data/habitatai/images/img_' + str(decoder)[0:6] + '.png',normalized_img_tensor, cmap=plt.cm.gray)
+    # """
+
     for obs_transform in obs_transforms:
         batch = obs_transform(batch)
         batch_all.append(copy.deepcopy(batch))
-        #added
-        if batch['rgb'].shape[-1]==1:
-            plt.imsave('/home/carsan/Data/habitatai/images/img_'+str(obs_transform)[0:6]+'.png', batch['rgb'][0,:,:,0].detach().cpu().numpy(), cmap=plt.cm.gray)
-        else:
-            plt.imsave('/home/carsan/Data/habitatai/images/img_'+str(obs_transform)[0:6]+'.png', batch['rgb'][0,:,:,:].detach().cpu().numpy(), cmap=plt.cm.gray)
 
+        # Added
+        # """
+        if batch['rgb'].shape[-1]==1:
+            new_image = batch['rgb'][0, :, :, :].detach().cpu().numpy()
+            min_val = np.min(new_image)
+            max_val = np.max(new_image)
+            if (max_val - min_val) != 0:
+                normalized_img_tensor = (new_image - min_val) / (
+                        max_val - min_val)
+            else:
+                normalized_img_tensor = new_image / np.max(new_image)
+            plt.imsave('/home/carsan/Data/habitatai/images/img_'+str(obs_transform)[0:6]+'.png', normalized_img_tensor, cmap=plt.cm.gray)
+        else:
+            new_image = batch['rgb'][0,:,:,0].detach().cpu().numpy()
+            min_val = np.min(new_image)
+            max_val = np.max(new_image)
+            if (max_val - min_val) != 0:
+                normalized_img_tensor = (new_image - min_val) / (max_val - min_val)
+            else:
+                normalized_img_tensor = new_image / np.max(new_image)
+            plt.imsave('/home/carsan/Data/habitatai/images/img_'+str(obs_transform)[0:6]+'.png', normalized_img_tensor, cmap=plt.cm.gray)
+        # """
     return batch, batch_all
 
 @baseline_registry.register_obs_transformer()

habitat-baselines/habitat_baselines/rl/ppo/policy.py
@@ -260,10 +260,9 @@ class NetPolicy(nn.Module, Policy):
         act,
         deterministic=False,
     ):
-        features, rnn_hidden_states, _, _, _, _, _ = self.net(
+        features, rnn_hidden_states, _, _, _, _, _ = self.net( # Goes to forward function in resnet_policy.py
             obs_transforms, observations, rnn_hidden_states, prev_actions,
             masks, decoder, act
-            # obs_transforms, observations, rnn_hidden_states, prev_actions, masks, act
         )
         distribution = self.action_distribution(features)
         value = self.critic(features)
@@ -279,7 +278,7 @@ class NetPolicy(nn.Module, Policy):
         action_log_probs = distribution.log_probs(action)
         # return value, action, action_log_probs, rnn_hidden_states
         # The simple return statement does not create the appropriate object to
-        # return action_data.env_actions
+        # later on return action_data.env_actions
         return PolicyActionData(
             values=value,
             actions=action,
@@ -297,9 +296,8 @@ class NetPolicy(nn.Module, Policy):
 
     # Start E2E block (function added)
     def get_value_e2e(self, obs_transforms, observations, rnn_hidden_states, prev_actions, masks, decoder, act):
-        features, _ , _ ,_, _, _, _ = self.net(
+        features, _ , _ ,_, _, _, _ = self.net( # Goes to forward function in resnet_policy.py (same as act_e2e does)
             obs_transforms, observations, rnn_hidden_states, prev_actions, masks, decoder, act
-            # obs_transforms, observations, rnn_hidden_states, prev_actions, masks, act
         )
         return self.critic(features)
     # End E2E block

habitat-baselines/habitat_baselines/rl/ppo/ppo.py
@@ -56,8 +56,9 @@ loss_function = CustomLoss(recon_loss_type='mse',
 class PPO(nn.Module, Updater):
     entropy_coef: Union[float, LagrangeInequalityCoefficient]
 
+    # Changed for E2E
     @classmethod
-    def from_config(cls, actor_critic: NetPolicy, config):
+    def from_config(cls, actor_critic: NetPolicy, config, obs_transforms=None, decoder=None):
         config = {k.lower(): v for k, v in config.items()}
         param_dict = dict(actor_critic=actor_critic)
         sig = inspect.signature(cls.__init__)
@@ -71,6 +72,10 @@ class PPO(nn.Module, Updater):
 
             param_dict[p.name] = config[p.name]
 
+        # Start E2E block (allow decoder and obs_transforms to exist in the init of ppo already
+        param_dict["decoder"] = decoder
+        param_dict["obs_transforms"] = obs_transforms
+        # End E2E block
         return cls(**param_dict)
 
     def __init__(
@@ -96,6 +101,8 @@ class PPO(nn.Module, Updater):
         super().__init__()
 
         self.actor_critic = actor_critic
+        self.obs_transforms = obs_transforms
+        self.decoder = decoder
 
         self.clip_param = clip_param
         self.ppo_epoch = ppo_epoch
@@ -109,23 +116,8 @@ class PPO(nn.Module, Updater):
 
         self.device = next(actor_critic.parameters()).device
 
-        # Start E2E block
-        if all(isinstance(item, ObservationTransformer) for item in obs_transforms):
-            pass
-        else:
-            path_config = str(Path('~/Internship/PyCharm_projects/habitat-phosphenes/'
-                 'ppo_pointnav_phosphenes_complete.yaml').expanduser())
-            _config = phosphenes.get_config(path_config)
-
-            obs_transforms = get_active_obs_transforms(_config)
-
-            obs_trans_cls = baseline_registry.get_obs_transformer("E2E_Decoder")
-            if obs_trans_cls is None:
-                raise ValueError(
-                    f"Unkown ObservationTransform with name E2E_Decoder."
-                )
-            decoder = obs_trans_cls.from_config({"type":"E2E_Decoder"})
-        # End E2E block
+        # From here, i deleted the code to load again the obs_transforms,
+        # if everything works, now they come from ppo_trainer through single_agent_access_mgr
 
         if (
             use_adaptive_entropy_pen
@@ -145,13 +137,7 @@ class PPO(nn.Module, Updater):
 
         self.use_normalized_advantage = use_normalized_advantage
 
-        params = list(filter(lambda p: p.requires_grad, self.parameters()))
-
-        self.non_ac_params = [
-            p
-            for name, p in self.named_parameters()
-            if not name.startswith("actor_critic.")
-        ]
+        params = list(filter(lambda p: p.requires_grad, self.parameters())) # Added the actor_critic word to not include decoder twice
 
         # Start E2E block
 
@@ -163,28 +149,19 @@ class PPO(nn.Module, Updater):
         # Or, how can I reverse engineer the way it used to work in the previous version of habitat? I cannot run that code
 
         # E2E block
-        parameter_list = []
-
         # In general, all the parameters have required_grad set to true, at least for Encoder, actor_critic, and decoder.
-        for transform in obs_transforms: # Around 21 parameters tracked (Simulator does not have parameters tracked)
+        for transform in self.obs_transforms: # Around 22 parameters tracked (Simulator does not have parameters tracked)
             for p in transform.parameters():
                 if hasattr(p, "requires_grad") and p.requires_grad:
-                    print(str(transform)[:6])
-                    parameter_list.append(p)
+                    #print(str(transform)[:6])
+                    params.append(p)
 
-        for p in actor_critic.parameters(): # Around 74 parameters tracked (95 together with the transforms)
-            if hasattr(p, "requires_grad") and p.requires_grad:
-                print('actor')
-                parameter_list.append(p)
+        # for p in self.decoder.parameters(): # 22 parameters
+        #     if hasattr(p, "requires_grad") and p.requires_grad:
+                #print('decoder')
+                # params.append(p)
 
-        for p in decoder.parameters(): # 22 parameters
-            if hasattr(p, "requires_grad") and p.requires_grad:
-                print('decoder')
-                parameter_list.append(p)
-
-        print('whole list', len(parameter_list))
-
-        params = parameter_list
+        print('whole list', len(params))
         # End E2E block
 
         if len(params) > 0:
@@ -332,16 +309,14 @@ class PPO(nn.Module, Updater):
         total_loss = torch.stack(all_losses).sum()
 
         total_loss = self.before_backward(total_loss)
-        # torch.use_deterministic_algorithms(False)
         total_loss.backward()
-        # torch.use_deterministic_algorithms(True)
         self.after_backward(total_loss)
 
         grad_norm = self.before_step()
         self.optimizer.step()
         self.after_step()
 
-        with inference_mode():
+        with torch.no_grad():
             if "is_coeffs" in batch:
                 record_min_mean_max(batch["is_coeffs"], "ver_is_coeffs")
             record_min_mean_max(orig_values, "value_pred")
@@ -384,7 +359,7 @@ class PPO(nn.Module, Updater):
 
     # Start E2E block
     def _update_from_batch_e2e(self, batch, epoch, rollouts: RolloutStorage,
-                               learner_metrics, obs_transforms, decoder):
+                               learner_metrics):
         """
                 Performs a gradient update from the minibatch.
                 """
@@ -414,14 +389,14 @@ class PPO(nn.Module, Updater):
             update_reconstructions
         ) = self._evaluate_actions_e2e(
             # this is the where the forward pass happens
-            obs_transforms,
+            self.obs_transforms,
             batch["observations_orig"],
             # batch["observations"],
             batch["recurrent_hidden_states"],
             batch["prev_actions"],
             batch["masks"],
             batch["actions"],
-            decoder,
+            self.decoder,
             batch.get("rnn_build_seq_info", None),
         )
 
@@ -464,8 +439,6 @@ class PPO(nn.Module, Updater):
         else:
             mean_fn = torch.mean
 
-        self.optimizer.zero_grad()
-
         action_loss, value_loss, dist_entropy = map(
             mean_fn,
             (action_loss, value_loss, dist_entropy),
@@ -489,25 +462,33 @@ class PPO(nn.Module, Updater):
             phosphenes=update_phosphenes,
             reconstruction=update_reconstructions)
 
-        stim_loss = stim_loss.to(torch.float32)
+        stim_loss = stim_loss.to(torch.float32)  # usually super high, around 16000
         recon_loss = recon_loss.to(torch.float32)
         spars_loss = spars_loss.to(torch.float32)
 
-        total_loss = weight_loss_ppo * ppo_loss + (
-                1 - weight_loss_ppo) * torch.mean(stim_loss)
+        total_loss = weight_loss_ppo * ppo_loss + (1 - weight_loss_ppo) * torch.mean(stim_loss)
         total_loss = total_loss.to(torch.float32)
 
+        self.optimizer.zero_grad() # Not used anymore, I have set grads to zero at the beginning already
+
         self.before_backward(total_loss)
-        # torch.use_deterministic_algorithms(False)
         total_loss.backward()
-        # torch.use_deterministic_algorithms(True)
         self.after_backward(total_loss)
+
+        # Hard code the update of optimizer
+        # counter = 76
+        # for param in self.decoder.parameters():
+        #     self.optimizer.param_groups[0]['params'][counter].grad = param.grad
+        #     counter = counter + 1
+        # for param in self.obs_transforms[1].parameters():
+        #     self.optimizer.param_groups[0]['params'][counter].grad = param.grad
+        #     counter = counter + 1
 
         grad_norm = self.before_step()
         self.optimizer.step()
         self.after_step()
 
-        with inference_mode():
+        with torch.no_grad():
             if "is_coeffs" in batch:
                 record_min_mean_max(batch["is_coeffs"], "ver_is_coeffs")
             record_min_mean_max(orig_values, "value_pred")
@@ -582,7 +563,7 @@ class PPO(nn.Module, Updater):
 
         self._set_grads_to_none()
 
-        with inference_mode():
+        with torch.no_grad():
             return {
                 k: float(
                     torch.stack(
@@ -594,8 +575,7 @@ class PPO(nn.Module, Updater):
 
     # Start E2E block
     def update_e2e(self, rollouts: RolloutStorage,
-                   obs_transforms: Iterable[ObservationTransformer],
-                   decoder) -> Dict[str, float]:
+                   ) -> Dict[str, float]:
 
         advantages = self.get_advantages(rollouts)
 
@@ -608,14 +588,14 @@ class PPO(nn.Module, Updater):
             )
 
             for _bid, batch in enumerate(data_generator):
-                self._update_from_batch_e2e(batch, epoch, rollouts, learner_metrics, obs_transforms, decoder)
+                self._update_from_batch_e2e(batch, epoch, rollouts, learner_metrics)
 
             profiling_wrapper.range_pop()  # PPO.update epoch
 
-        self._set_grads_to_none()
+        # self._set_grads_to_none()
 
         # The decision of which return method to use or if combine them depends on which variable I record in the learner_metrics array
-        with inference_mode():
+        with torch.no_grad():
             return {
                 k: float(
                     torch.stack(

habitat-baselines/habitat_baselines/rl/ppo/ppo_trainer.py
@@ -132,6 +132,10 @@ class PPOTrainer(BaseRLTrainer):
         return t.to(device=orig_device)
 
     def _create_obs_transforms(self):
+        # I need to create the transformations here because if not then when
+        # initiating training the transforms and decoder does not exist,
+        # I also need to move it to cuda (device) so I don't get errors in
+        # the phosphenes
         self.obs_transforms = get_active_obs_transforms(self.config)
 
         # Start E2E block
@@ -143,6 +147,10 @@ class PPOTrainer(BaseRLTrainer):
                     f"Unkown ObservationTransform with name E2E_Decoder."
                 )
             self.decoder = obs_trans_cls.from_config({"type": "E2E_Decoder"})
+
+            self.decoder.to(self.device)
+            self.obs_transforms[1].model.to(self.device)
+            self.obs_transforms[2].gaussian.to(self.device)
         # End E2E block
 
         self._env_spec.observation_space = apply_obs_transforms_obs_space(
@@ -154,7 +162,6 @@ class PPOTrainer(BaseRLTrainer):
         Sets up the AgentAccessMgr. You still must call `agent.post_init` after
         this call. This only constructs the object.
         """
-
         self._create_obs_transforms()
         return baseline_registry.get_agent_access_mgr(
             self.config.habitat_baselines.rl.agent.type
@@ -166,6 +173,8 @@ class PPOTrainer(BaseRLTrainer):
             resume_state=resume_state,
             num_envs=self.envs.num_envs,
             percent_done_fn=self.percent_done,
+            obs_transforms=self.obs_transforms, # Added E2E (for only 1 instance)
+            decoder=self.decoder, # Added E2E (for only 1 instance)
             **kwargs,
         )
 
@@ -275,21 +284,6 @@ class PPOTrainer(BaseRLTrainer):
         logger.add_filehandler(self.config.habitat_baselines.log_file)
         self._agent = self._create_agent(resume_state)
 
-        # Start E2E block
-        # This is so when _create_agent is called, the E2E stuff is part of
-        # the resume state dict. It is an attempt to immitate the old functions
-        # of DDPPO or PPO to initialize providing all the parameters one by one,
-        # just like it is in Burcu's version line 267 onwards
-
-        # Update, I don't think it is necessary, I think it is the problem I
-        # already solved in PPO init related to the existance of obs_transforms
-        # and decoder. But still, I'll keep it for now as a comment.
-
-        # if 'Encoder' in str(self.obs_transforms[1]):
-        #     resume_state["decoder"] = E2E_Decoder()
-        #     resume_state["obs_transforms"] = self.obs_transforms
-        # End E2E block
-
         if self._is_distributed:
             self._agent.updater.init_distributed(find_unused_params=False)  # type: ignore
         self._agent.post_init()  # Rollouts originally created inside here
@@ -303,10 +297,10 @@ class PPOTrainer(BaseRLTrainer):
         batch = batch_obs(observations, device=self.device)
 
         # Changed for E2E block ("with" statement)
-        # with torch.no_grad():
-        #     batch = apply_obs_transforms_batch(batch, self.obs_transforms)  # type: ignore
+        with torch.no_grad():
+            batch = apply_obs_transforms_batch(batch, self.obs_transforms)  # type: ignore
 
-        batch = apply_obs_transforms_batch(batch, self.obs_transforms)  # type: ignore
+        # batch = apply_obs_transforms_batch(batch, self.obs_transforms)  # type: ignore
 
         if self._is_static_encoder:
             assert isinstance(self._agent.actor_critic, NetPolicy)
@@ -545,10 +539,10 @@ class PPOTrainer(BaseRLTrainer):
         batch = batch_obs(observations, device=self.device)
         # Start E2E block
         batch_orig = copy.deepcopy(batch)
-        # with torch.no_grad():
-        #     batch = apply_obs_transforms_batch(batch, self.obs_transforms)  # This line remained the same. I was out of the "with" block
+        with torch.no_grad():
+            batch = apply_obs_transforms_batch(batch, self.obs_transforms)  # This line remained the same. I was out of the "with" block
         # End E2E block
-        batch = apply_obs_transforms_batch(batch, self.obs_transforms) # Original (No E2E)
+        # batch = apply_obs_transforms_batch(batch, self.obs_transforms) # Original (No E2E)
 
         rewards = torch.tensor(
             rewards_l,
@@ -670,7 +664,7 @@ class PPOTrainer(BaseRLTrainer):
         # Start E2E block
         if 'Encoder' in str(self.obs_transforms[1]):
             losses = self._agent.updater.update_e2e(
-                self._agent.rollouts, self.obs_transforms, self.decoder
+                self._agent.rollouts
             )
         else:
             losses = self._agent.updater.update(self._agent.rollouts)
@@ -933,7 +927,7 @@ class PPOTrainer(BaseRLTrainer):
                 if self._is_distributed:
                     self.num_rollouts_done_store.add("num_done", 1)
 
-                losses = self._update_agent()
+                losses = self._update_agent() # From here it goes to the update_e2e functions in ppo.py
 
                 self.num_updates_done += 1
                 losses = self._coalesce_post_step(
@@ -978,7 +972,7 @@ class PPOTrainer(BaseRLTrainer):
                                         value.data.cpu(),
                                         self.num_updates_done)
 
-                        for tag, value in self._agent.actor_critic.net.named_parameters():
+                        for tag, value in self._agent.actor_critic.named_parameters():
                             # print('tag', tag)
                             if value.grad is not None:
                                 # print('grad tag', tag)
@@ -1025,12 +1019,18 @@ class PPOTrainer(BaseRLTrainer):
         Returns:
             None
         """
-        # Start E2E block
         checkpoint_path_phos = ""
-        for i in range(len(checkpoint_path.split("_")) - 1):
-            checkpoint_path_phos += checkpoint_path.split("_")[i] + "_"
-        checkpoint_path_phos += "phos." + checkpoint_path.split(".")[
-            -2] + ".pth"
+        aux_directory, aux_filename = os.path.split(checkpoint_path)
+        if "_phos" not in aux_filename:
+            first_dot_index = aux_filename.find('.')
+            if first_dot_index != -1:
+                checkpoint_path_phos = aux_directory + "/" + aux_filename[:first_dot_index] + "_phos" + aux_filename[first_dot_index:]
+            else:
+                checkpoint_path_phos = checkpoint_path
+            print('checkpoint_path_phos', checkpoint_path_phos)
+        else:
+            checkpoint_path_phos = checkpoint_path
+
         print('checkpoint_path_phos', checkpoint_path_phos)
 
         self.checkpoint_path = checkpoint_path
@@ -1108,7 +1108,7 @@ class PPOTrainer(BaseRLTrainer):
         # Start E2E block
         # batch = apply_obs_transforms_batch(batch, self.obs_transforms)  # type: ignore
         with torch.no_grad():
-            batch, _ = apply_obs_transforms_batch_video(batch, self.obs_transforms)
+            batch, _ = apply_obs_transforms_batch_video(batch, self.obs_transforms, self.decoder)
         # End E2E block
 
         current_episode_reward = torch.zeros(
@@ -1269,7 +1269,7 @@ class PPOTrainer(BaseRLTrainer):
 
             # Start E2E block
             with torch.no_grad():
-                batch, _ = apply_obs_transforms_batch_video(batch, self.obs_transforms)  # type: ignore
+                batch, _ = apply_obs_transforms_batch_video(batch, self.obs_transforms, self.decoder)  # type: ignore
             # End E2E block
 
             not_done_masks = torch.tensor(