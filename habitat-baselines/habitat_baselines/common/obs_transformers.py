E2E implementation working. -Vanishing gradients fixed through Xavier weights initialization. -Normalization for loss computations. -Encoder-Simulator-Decoder interactions working. -Reconstruction loss behaving as expected.

habitat-baselines/habitat_baselines/common/obs_transformers.py
@@ -1246,54 +1246,32 @@ def apply_obs_transforms_batch_video(
     decoder
 ) -> Tuple[Dict[str, torch.Tensor], List[Dict[str, torch.Tensor]]]:
     batch_all=[]
-    # """
-    if isinstance(decoder, ObservationTransformer):
-        reconstruction = decoder(batch)
-        if reconstruction['rgb'].shape[-1] == 1:
-            new_image_recon = reconstruction['rgb'][0, :, :, 0].detach().cpu().numpy()
-            min_val = np.min(new_image_recon)
-            max_val = np.max(new_image_recon)
-            if (max_val - min_val) != 0:
-                normalized_img_tensor = (new_image_recon - min_val) / (max_val - min_val)
-            else:
-                normalized_img_tensor = new_image_recon / np.max(new_image_recon)
-        else:
-            new_image_recon = reconstruction['rgb'][0, :, :, :].detach().cpu().numpy()
-            min_val = np.min(new_image_recon)
-            max_val = np.max(new_image_recon)
-            if (max_val - min_val) != 0:
-                normalized_img_tensor = (new_image_recon - min_val) / (max_val - min_val)
-            else:
-                normalized_img_tensor = new_image_recon / np.max(new_image_recon)
-        plt.imsave('/home/carsan/Data/habitatai/images/img_' + str(decoder)[0:6] + '.png',normalized_img_tensor, cmap=plt.cm.gray)
-    # """
 
     for obs_transform in obs_transforms:
         batch = obs_transform(batch)
         batch_all.append(copy.deepcopy(batch))
-
         # Added
         # """
-        if batch['rgb'].shape[-1]==1:
+        if batch['rgb'].shape[-1]==1: # Gray and Encoder
             new_image = batch['rgb'][0, :, :, :].detach().cpu().numpy()
-            min_val = np.min(new_image)
-            max_val = np.max(new_image)
-            if (max_val - min_val) != 0:
-                normalized_img_tensor = (new_image - min_val) / (
-                        max_val - min_val)
-            else:
-                normalized_img_tensor = new_image / np.max(new_image)
-            plt.imsave('/home/carsan/Data/habitatai/images/img_'+str(obs_transform)[0:6]+'.png', normalized_img_tensor, cmap=plt.cm.gray)
-        else:
-            new_image = batch['rgb'][0,:,:,0].detach().cpu().numpy()
-            min_val = np.min(new_image)
-            max_val = np.max(new_image)
-            if (max_val - min_val) != 0:
-                normalized_img_tensor = (new_image - min_val) / (max_val - min_val)
-            else:
-                normalized_img_tensor = new_image / np.max(new_image)
-            plt.imsave('/home/carsan/Data/habitatai/images/img_'+str(obs_transform)[0:6]+'.png', normalized_img_tensor, cmap=plt.cm.gray)
+            plt.imsave('/home/carsan/Data/habitatai/images/img_'+str(obs_transform)[0:6]+'.png', np.squeeze(new_image), cmap=plt.cm.gray)
+        else: # Simulator
+            new_image = batch['rgb'][0,:,:,:].detach().cpu().numpy()
+            # min_val = np.min(new_image)
+            # max_val = np.max(new_image)
+            # if (max_val - min_val) != 0:
+            #     new_image = (new_image - min_val) / (max_val - min_val)
+            # else:
+            #     new_image = new_image / np.max(new_image)
+            plt.imsave('/home/carsan/Data/habitatai/images/img_'+str(obs_transform)[0:7]+'.png', new_image, cmap=plt.cm.gray)
         # """
+
+    # """
+    if isinstance(decoder, ObservationTransformer):
+        reconstruction = decoder(batch.copy())
+        new_image_recon = reconstruction['rgb'][0, :, :, 0].detach().cpu().numpy()
+        plt.imsave('/home/carsan/Data/habitatai/images/img_' + str(decoder)[0:7] + '.png', new_image_recon, cmap=plt.cm.gray)
+    # """
     return batch, batch_all
 
 @baseline_registry.register_obs_transformer()

habitat-baselines/habitat_baselines/rl/ppo/policy.py
@@ -357,7 +357,7 @@ class NetPolicy(nn.Module, Policy):
         decoder,
         rnn_build_seq_info: Dict[str, torch.Tensor]
     ):
-        features, rnn_hidden_states, aux_loss_state, observations_gray, stimulations, phosphenes, reconstructions = self.net.forward(
+        features, rnn_hidden_states, aux_loss_state, observations_gray, stimulations, phosphenes, reconstructions = self.net.forward( # Goes to forward function in resnet_policy.py
             obs_transforms, observations_orig, rnn_hidden_states,
             prev_actions, masks, decoder, act=False, rnn_build_seq_info=rnn_build_seq_info
         )

habitat-baselines/habitat_baselines/rl/ppo/ppo.py
@@ -469,7 +469,7 @@ class PPO(nn.Module, Updater):
         total_loss = weight_loss_ppo * ppo_loss + (1 - weight_loss_ppo) * torch.mean(stim_loss)
         total_loss = total_loss.to(torch.float32)
 
-        self.optimizer.zero_grad() # Not used anymore, I have set grads to zero at the beginning already
+        self.optimizer.zero_grad() # Not used anymore, I have set grads to zero at the beginning already, but it does not matter
 
         self.before_backward(total_loss)
         total_loss.backward()